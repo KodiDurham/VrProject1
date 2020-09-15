734c3bde1af149be7e6fcf6babb0691e187f24da
KodiD <40518298+KodiD@users.noreply.github.com>
2020-09-15 19:22:16 -0400

    Added a particale effect to indicate movement and  implemented a good movement and bad movement color.

--- Assets/Scripts/rayShot.cs
+++ Assets/Scripts/rayShot.cs
@@ -1,20 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class rayShot : MonoBehaviour
 {
+    public GameObject particleForMove;
+    public Gradient goodHit;
+    public Gradient badHit;
+
+
     public Camera camera;
     RaycastHit hit;
 
     float xRot = 0;
     float yRot = 0;
 
+    bool isTargetMode = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        particleForMove.SetActive(false);
     }
 
     // Update is called once per frame
@@ -33,9 +43,22 @@ public class rayShot : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRot, yRot, 0f);
 
+        if (Input.GetMouseButtonDown(0) && isTargetMode==false)
+            isTargetMode = true;
 
-        if (Input.GetMouseButtonDown(0))
+
+        if (isTargetMode/*Input.GetMouseButtonDown(0)*/)
         {
+
+            particleForMove.SetActive(true);
+
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                isTargetMode = false;
+                particleForMove.SetActive(false);
+
+            }
 
             Ray ray = new Ray(camera.transform.position, camera.transform.forward);
             //Ray ray = camera.ScreenPointToRay(Input.mousePosition);
@@ -43,11 +66,24 @@ public class rayShot : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 Transform objectHit = hit.transform;
+                ParticleSystem indicator = particleForMove.GetComponent<ParticleSystem>();
+
+                var gColor =  indicator.colorOverLifetime;
+                gColor.enabled = true;
 
                 if (objectHit.tag == "ground")
+                    gColor.color = goodHit;
+                else
+                    gColor.color = badHit;
+
+                particleForMove.transform.position = new Vector3(hit.point.x, particleForMove.transform.position.y, hit.point.z); //hit.transform.position;
+
+                if (objectHit.tag == "ground" && Input.GetMouseButtonUp(0))
                 {
                     Debug.Log(Input.mousePosition);
                     this.transform.position = new Vector3(hit.point.x, this.transform.position.y, hit.point.z); //hit.transform.position;
+                    isTargetMode = false;
+                    particleForMove.SetActive(false);
                 }
 
             }
