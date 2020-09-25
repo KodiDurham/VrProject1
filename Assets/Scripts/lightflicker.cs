a7be28f03b3dac24ceee4e9a78f2eaf721102724
KodiD <40518298+KodiD@users.noreply.github.com>
2020-09-25 19:15:56 -0400

    added doors light bulbs and added heartbeat and flat line

---
+++ Assets/Scripts/lightflicker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightflicker : MonoBehaviour
+{
+
+    public Light lightToF;
+    public float minLightLevel=0;
+    public float maxLightLevel=6;
+    public int smooth = 5;
+    public AudioSource sound;
+    public float cutSoundAt = 2;
+
+    public GameObject lightOn;
+    public GameObject lightOff;
+
+    Queue<float> smoothQ;
+    float lSum=0;
+
+
+    public void Reset()
+    {
+        smoothQ.Clear();
+        lSum = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smoothQ = new Queue<float>(smooth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (smoothQ.Count >= smooth)
+        {
+            lSum -= smoothQ.Dequeue();
+        }
+
+        float RanVal = Random.Range(minLightLevel, maxLightLevel);
+        smoothQ.Enqueue(RanVal);
+        lSum += RanVal;
+
+        float avgLight = lSum / (float)smoothQ.Count;
+
+        if (avgLight <= cutSoundAt)
+        {
+            sound.enabled = false;
+            lightOn.SetActive(false);
+            lightOff.SetActive(true);
+        }
+        else
+        {
+            sound.enabled = true;
+            lightOn.SetActive(true);
+            lightOff.SetActive(false);
+        }
+        lightToF.intensity = avgLight;
+
+    }
+}
---
+++ Assets/Scripts/triggerAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerAudio : MonoBehaviour
+{
+
+    public AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound.enabled = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach(ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        Debug.Log(""+collision.gameObject.name);
+            sound.enabled = true;
+    }
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    sound.enabled = false;
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
